c846b1a303f0246d524ebd6443ed6e220a23cc85
arkorede <israelashaolu2000@gmail.com>
2022-10-17 21:35:22 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import login from './login.png'
+import swal from 'sweetalert'
+import React, { useState } from 'react'
+
 
 function App() {
+
+  const [user, setUser] = useState('')
+  const [pass, setPass] = useState('')
+
+  const handleValidate = (e) => {
+    e.preventDefault()
+    if (user == 'reactjs12@gmail.com' && pass == 123456) {
+      swal('Succesful login', 'Welcome ReactJS', 'success')
+    } else {
+      swal("Wrong Credentials", "Please try again", "error")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <h1>Authentication Portal</h1>
+      <div className='row'>
+        <div className='col-md-6'>
+          <img className='float-end' src={login} width="600" />
+        </div>
+        <div className='col-md-6 my-auto bg-dark p-5 rounded'>
+          <form onSubmit={handleValidate}>
+            <div class="mb-3">
+              <label className="form-label">Email address</label>
+              <input type="email" value={user} className="form-control" placeholder="Enter email" onChange={(e) => setUser(e.target.value)} />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Password</label>
+              <input type="password" value={pass} className="form-control" placeholder='Enter password' onChange={(e) => setPass(e.target.value)} />
+            </div>
+
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </div>
+
+      </div>
     </div>
   );
 }
